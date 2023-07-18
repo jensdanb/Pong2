ce0bc8de47bbce93fcf8f614433ac9f7ec9296da
jensdanb <56897399+jensdanb@users.noreply.github.com>
2023-07-18 17:31:05 +0200

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -5,9 +5,5 @@ import GUI (playPong)
 
 main :: IO ()
 main = do
-
-
     putStrLn ("\n" ++ "Launching GUI...")
-
-    -- let windowSize = (1200, 800) :: (Int, Int)  -- To be move into Launcher
     playPong
